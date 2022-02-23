b862aee9806d69e1db3f36342cddafabdc98e216
marcianomendes <nmendes94@hotmail.com>
2022-02-23 14:12:04 -0300

    Begin cotrol with authentication

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jsonwebtoken = require('jsonwebtoken');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var jwtSecret = "fskfjsflsfsfsrwetskslgçsdgsgçs~gsgw9trw_";
+
+function auth(request, response, next) {
+    const authtoken = request.headers['authorization'];
+    if (authtoken != undefined) {
+        const bearer = authtoken.split(' ');
+        var token = bearer[1];
+        jsonwebtoken.verify(token, jwtSecret, (error, data) => {
+            if (error) {
+                response.status(401);
+                response.json({ err: "Token inválido" });
+                return;
+            }
+
+            request.token = token;
+            request.loggedUser = { id: data.id, email: data.email };
+            console.log(data);
+            next();
+        });
+
+        return;
+    }
+
+    response.sendStatus = 401;
+    response.json({ err: "Token inválido" });
+}
 
 var DataBase = {
     games: [
@@ -28,11 +56,25 @@ var DataBase = {
             price: 78
         }
     ],
+    users: [
+        {
+            id: 1,
+            name: 'joao',
+            email: 'joao@joao.com',
+            password: 'joao123'
+        },
+        {
+            id: 2,
+            name: 'maria',
+            email: 'maria@maria.com',
+            password: 'maria123'
+        }
+    ]
 }
 
-app.get("/games", (request, response) => {
+app.get("/games", auth, (request, response) => {
     response.statuscode = 200;
-    response.json(DataBase.games);
+    response.json({user: request.loggedUser, games: DataBase.games});
 });
 
 app.get("/game/:id", (request, response) => {
@@ -51,7 +93,7 @@ app.get("/game/:id", (request, response) => {
     }
 
     response.sendStatus(404);
-})
+});
 
 app.post("/game", (request, response) => {
     var { title, year, price } = request.body;
@@ -68,7 +110,7 @@ app.post("/game", (request, response) => {
     });
 
     response.sendStatus(200);
-})
+});
 
 app.delete("/game/:id", (request, response) => {
     if (isNaN(request.params.id)) {
@@ -86,7 +128,7 @@ app.delete("/game/:id", (request, response) => {
     }
 
     response.sendStatus(404);
-})
+});
 
 app.put("/game/:id", (request, response) => {
     if (isNaN(request.params.id)) {
@@ -96,7 +138,6 @@ app.put("/game/:id", (request, response) => {
 
     var id = parseInt(request.params.id);
     game = DataBase.games.find(game => game.id == id);
-
     if (game != undefined) {
         var { title, year, price } = request.body;
         if (title != undefined) {
@@ -116,9 +157,44 @@ app.put("/game/:id", (request, response) => {
     }
 
     response.sendStatus(404);
+});
+
+app.post("/auth", (request, response) => {
+    var { email, password } = request.body;
+    if (email == undefined) {
+        response.sendStatus(400);
+        response.json({ err: "E-mail enviado é inválido!" });
+        return;
+    }
+
+    user = DataBase.users.find(user => user.email == email);
+
+    if (user != undefined) {
+        if (password == user.password) {
+            jsonwebtoken.sign({ id: user.id, email: user.email }, jwtSecret, { expiresIn: "24h" }, (err, token) => {
+                if (err) {
+                    response.statuscode = 400;
+                    response.json({ err: "Falha interna!" });
+                    return;
+                }
+
+                response.json({ token: token });
+                response.statuscode = 200;
+            });
+
+            return;
+        }
+
+        response.statuscode = 401;
+        response.json({ err: "Credenciais Inválidas!" })
+        return;
+    }
+
+    response.statuscode = 404;
+    response.json({ err: "E-mail enviado não foi encontradao na base de dados!" });
 })
 
 
 app.listen(45678, () => {
-    console.log('API Rodando')
+    console.log('API Rodando!')
 });
